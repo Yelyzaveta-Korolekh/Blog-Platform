a3fe12743ebefdf7fa1b2a1178976a7c5cab926c
Yelyzaveta-Korolekh <97096199+Yelyzaveta-Korolekh@users.noreply.github.com>
2025-01-08 19:23:15 +0200

    added styles for login form

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
-from django.contrib.auth import login
+from django.urls import reverse_lazy
 
 def index(request): 
     return render(request, 'blog/index.html')
@@ -18,7 +18,11 @@ def blog(request):
 
 class CustomLoginView(LoginView): 
     template_name = 'blog/registration/login.html'
+    
+    def get_success_url(self): 
+        return reverse_lazy('index')
 
 class SignupView(generic.CreateView):
     template_name = "blog/registration/signup.html"
     form_class = UserCreationForm
+    success_url = reverse_lazy('index')
