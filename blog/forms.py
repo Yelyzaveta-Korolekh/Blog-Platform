99750b470579336a2b87522a9718ef51c6ee8cd1
Yelyzaveta-Korolekh <97096199+Yelyzaveta-Korolekh@users.noreply.github.com>
2025-01-14 17:33:37 +0200

    Added styles for comments

--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,8 @@ class PostForm(forms.ModelForm):
         fields = ['title', 'content']
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField( widget=forms.Textarea(attrs={'placeholder': 'Напишіть свій коментар тут...'}), label='')
     class Meta:
         model = Comment
         fields = ['content']
+        
